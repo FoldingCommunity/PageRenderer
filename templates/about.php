3412aa7e67b2bfa578170639025ae12d36b732ab
Till Helge Helwig <thh@tshw.de>
2020-08-06 23:19:00 +0200

    Generate some dummy templates

---
+++ templates/about.php
@@ -0,0 +1,3 @@
+<div>{{{ About }}}</div>
+<div>{{{ Folding@home (FAH or F@h) }}}</div>
+<div>{{{ is a distributed computing project for simulating protein dynamics, including the process of protein folding and the movements of proteins implicated in a variety of diseases. It brings together citizen scientists who volunteer to run simulations of protein dynamics on their personal computers. Insights from this data are helping scientists to better understand biology, and providing new opportunities for developing therapeutics. }}}</div>
