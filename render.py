140bb3b95b3ba3bba34fefe91036bd14a3dc2e18
Till Helge Helwig <thh@tshw.de>
2020-08-08 11:30:06 +0200

    Improve output a little

--- render.py
+++ render.py
@@ -31,27 +31,25 @@ def render_pages(translations_path: Path, template_path: Path, output_path: Path
         with template.open('r', encoding='UTF-8') as tpl_file:
             raw_template = tpl_file.read()
             for (translation, path) in translations.items():
-                print(f'\tProcessing {translation}...')
                 try:
                     translation_file = translations_path / translation / (template.stem + '.po')
                     if not translation_file.exists():
-                        print(
-                            f'\t\tTranslation of file {template.name} for language {translation} is missing: {translation_file}')
+                        print(f'\t\t⚠ Translation of file {template.name} for language {translation} is missing: {translation_file}')
                         continue
                     with translation_file.open('r', encoding='UTF-8') as lang_file:
                         raw_messages = read_po(StringIO(lang_file.read()), charset='UTF-8')
                         messages = {}
                         for message in raw_messages:
                             messages[message.id] = _convert_markdown(message.string)
-                        print(messages)
                         output = _render_page(raw_template, messages)
                         output_file = output_path / translation / template.name
                         if not output_file.parent.exists():
                             output_file.parent.mkdir()
                         with open(output_path / translation / template.name, 'w', encoding='UTF-8') as out_file:
                             out_file.write(output)
+                            print(f'\t\t✅ Generated {out_file.name} for {translation}')
                 except Exception as e:
-                    print(f'\tERROR: {e}')
+                    print(f'\t\t⚠ Failed to generate for {translation}: {e}')
 
 
 if __name__ == '__main__':
